c366e15fac6a3b3a995de40fbb99687e56e1fe20
Deploy Action <deploy@example.com>
2021-09-18 09:45:52 -0700

    web-update

---
+++ route-home.chunk.f135c.esm.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{FDtd:function(e,o,n){"use strict";n.r(o);var s=n("hosL"),t="home__5d1wS";o.default=()=>Object(s.h)("div",{class:t},Object(s.h)("h1",null,"Home"),Object(s.h)("p",null,"This is the Home component."))}}]);
+//# sourceMappingURL=route-home.chunk.f135c.esm.js.map
